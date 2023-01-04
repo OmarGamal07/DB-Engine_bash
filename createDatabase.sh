c9bdd700ad4cae1371a582b264ad2c1ed96175d5
OmarGamal07 <120477394+OmarGamal07@users.noreply.github.com>
2023-01-04 16:46:26 +0200

    Add files via upload

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,47 +1,60 @@
+#!/bin/bash
 select choice in createDB listDB dropDB connectDB 
 do 
-# echo $choice
-# echo "Reply " $REPLY #Choice 
     case $REPLY in 
+    # choice create database 
         createDB)
         echo "CreateDB"
-            read -p "Enter Name of Folder : " name
+        # take name database from user
+            read -p "Enter Name of Database : " name
+            #check if this name already exist
             if [ -e $name ] ;then
                 echo "Already Exits : please enter another name "
             else 
+            #check regexs   contain character,number only  and start by character
                 if [[ $name =~ ^[a-zA-Z0-9]+$ && $name =~ ^[a-zA-Z]  ]];then
                      mkdir $name
+                     echo "database created";
                 else 
-                    echo "enter name not begin by number and not contain spectial char "     
+                    echo "Enter name not begin by number and not contain spectial char "     
                 fi
             fi 
+            . createDatabase.sh
         ;;  
         listDB) 
         echo "ListDB"
-          ls -F | grep "/"  
+          ls -F | grep "/"  ; #list dirctory database end by /
+          . createDatabase.sh
         ;;
         connectDB )
         echo "ConnectDB"
+           #take name database which user need connect
               read -p "Enter Name of Folder : " name
+              #check exist
               if [ -d $name ] ; then 
                   echo "I connect DB : "
                   cd $name ; 
-                  #do any thing and back
+                  #if exist go to tables select
+                  . tableDB.sh;
                   cd ..;
               else 
                echo "sorry this database not found"
               fi
+              . createDatabase.sh
         ;; 
         dropDB)
-          read -p "Enter Name of Folder : " name
+        #take name database which user need delete
+          read -p "Enter Name of database : " name
           if [ -d $name ] ; then 
                   rm -r $name 
                   echo " DB dropped : "
               else 
                echo "sorry this database not found"
               fi
+              . createDatabase.sh
         ;; 
         *) 
-         echo "default"
+         echo "command not found"
     esac
+    . createDatabase.sh
 done
